30d39e4823dda62c93517d32484b6cb4d9f5b206
Gustavo Duara <gugaduara@gmail.com>
2026-08-31 19:32:14 -0300

    adicionado DependencyInjection, ResultadoErro e instalado FluentResults na aplicação

--- LocadoraVeiculos.Aplicacao/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace LocadoraVeiculos.Aplicacao
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ LocadoraVeiculos.Aplicacao/DependencyInjection.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LocadoraDeVeiculos.Aplicacao;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddCamadaAplicacao(this IServiceCollection services, IConfiguration configuration)
+    {
+        var assembly = typeof(DependencyInjection).Assembly;
+
+        services.AddValidatorsFromAssembly(assembly);
+
+        var licenseKey = configuration["LUCKYPENNY_LICENSE_KEY"];
+
+        if (string.IsNullOrWhiteSpace(licenseKey))
+            throw new Exception("A variável LUCKYPENNY_LICENSE_KEY não foi fornecida.");
+
+        services.AddMediatR(config =>
+        {
+            config.LicenseKey = licenseKey;
+            config.RegisterServicesFromAssembly(assembly);
+        });
+
+        return services;
+    }
+}
