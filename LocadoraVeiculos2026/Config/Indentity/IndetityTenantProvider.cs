d75ab2c3307013288f9611f74bbbf6fab92d9673
Gustavo Duara <gugaduara@gmail.com>
2026-09-04 11:05:58 -0300

    feito TentantProvider e RefreshToken na Identity

--- LocadoraVeiculos2026/Config/Indentity/IndetityTenantProvider.cs
+++ LocadoraVeiculos2026/Config/Indentity/IndetityTenantProvider.cs
@@ -1,6 +1,26 @@
-﻿namespace LocadoraDeVeiculos.WebApi.Config.Indentity
+﻿using LocadoraDeVeiculos.Dominio.ModuloAutenticacao;
+
+namespace LocadoraDeVeiculos.WebApi.Config.Identity;
+
+public class IdentityTenantProvider(IHttpContextAccessor contextAccessor) : ITenantProvider
 {
-    public class IndetityTenantProvider
+    public Guid? EmpresaId
     {
+        get
+        {
+            var user = contextAccessor.HttpContext?.User;
+
+            var empresaIdClaim = user?.FindFirst("EmpresaId")?.Value;
+
+            if (Guid.TryParse(empresaIdClaim, out var empresaId))
+                return empresaId;
+
+            return null;
+        }
+    }
+
+    public bool EstaNoCargo(string cargo)
+    {
+        return contextAccessor.HttpContext?.User?.IsInRole(cargo) ?? false;
     }
 }
--- LocadoraVeiculos2026/Config/Indentity/RefreshTokenCookieService.cs
+++ LocadoraVeiculos2026/Config/Indentity/RefreshTokenCookieService.cs
@@ -1,6 +1,36 @@
-﻿namespace LocadoraDeVeiculos.WebApi.Config.Indentity
+﻿using LocadoraDeVeiculos.Dominio.ModuloAutenticacao;
+
+namespace LocadoraDeVeiculos.WebApi.Config.Identity;
+
+public static class RefreshTokenCookieService
 {
-    public class RefreshTokenCookieService
+    private static readonly string nome = "LocadoraDeVeiculos.RefreshToken";
+
+    public static void EnviarCookie(HttpResponse response, RefreshToken token)
     {
+        var options = new CookieOptions
+        {
+            HttpOnly = true,
+            Secure = true,
+            SameSite = SameSiteMode.None,
+            Expires = token.ExpiraEmUtc
+        };
+
+        response.Cookies.Append(nome, token.TokenHash, options);
+    }
+
+    public static void LimparCookie(HttpResponse response)
+    {
+        response.Cookies.Delete(nome, new CookieOptions
+        {
+            HttpOnly = true,
+            Secure = true,
+            SameSite = SameSiteMode.None
+        });
+    }
+
+    public static string? Get(HttpRequest request)
+    {
+        return request.Cookies[nome];
     }
 }
