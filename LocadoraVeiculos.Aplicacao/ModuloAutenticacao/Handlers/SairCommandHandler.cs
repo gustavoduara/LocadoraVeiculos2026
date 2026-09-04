7d43557c1bb3b77d85bc48262aad9135ab679250
Gustavo Duara <gugaduara@gmail.com>
2026-09-04 10:16:29 -0300

    feito SairCommandHandler

--- LocadoraVeiculos.Aplicacao/ModuloAutenticacao/Handlers/SairCommandHandler.cs
+++ LocadoraVeiculos.Aplicacao/ModuloAutenticacao/Handlers/SairCommandHandler.cs
@@ -12,4 +12,47 @@ using Microsoft.IdentityModel.Tokens;
 
 namespace LocadoraDeVeiculos.Aplicacao.ModuloAutenticacao.Handlers;
 
-public class SairCommandHandler();
+public class SairCommandHandler(
+    AppDbContext dbContext,
+    UserManager<Usuario> userManager,
+    RefreshTokenProvider refreshTokenProvider,
+    ILogger<SairCommandHandler> logger
+) : IRequestHandler<SairCommand, Result>
+{
+    public async Task<Result> Handle(SairCommand command, CancellationToken cancellationToken)
+    {
+        try
+        {
+            var token = await dbContext.RefreshTokens
+                .FirstOrDefaultAsync(t => t.TokenHash == command.RefreshTokenHash, cancellationToken);
+
+            if (token is null)
+                throw new SecurityTokenException("O token de rotação não foi encontrado.");
+
+            var usuarioId = token.UsuarioId;
+
+            await refreshTokenProvider.RevogarTokensUsuarioAsync(usuarioId, "Logout");
+
+            var usuarioEncontrado = await userManager.FindByIdAsync(usuarioId.ToString());
+
+            if (usuarioEncontrado is null)
+                return Result.Fail(ResultadosErro.RegistroNaoEncontradoErro("Não foi possível encontrar o usuário requisitado."));
+
+            usuarioEncontrado.AccessTokenVersionId = Guid.NewGuid();
+
+            await dbContext.SaveChangesAsync(cancellationToken);
+
+            return Result.Ok();
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(
+                ex,
+                "Ocorreu um erro durante o logout {@Command}.",
+                command
+            );
+
+            return Result.Fail(ResultadosErro.ExcecaoInternaErro(ex));
+        }
+    }
+}
