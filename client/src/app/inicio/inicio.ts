f5d7d40dc4fd62dc4873eb1eb4a0ce929d335cbb
Gustavo Duara <gugaduara@gmail.com>
2026-09-09 16:15:27 -0300

    editado style.scss e inicio

--- client/src/app/inicio/inicio.ts
+++ client/src/app/inicio/inicio.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
+import { map } from 'rxjs';
+
+import { AsyncPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
 
+import { AuthService } from '../auth/auth.service';
+
 @Component({
     selector: 'app-inicio',
-    imports: [MatCardModule, MatButtonModule, MatIconModule, MatDividerModule, MatTooltipModule],
+    imports: [
+        MatCardModule,
+        MatButtonModule,
+        MatIconModule,
+        MatDividerModule,
+        MatTooltipModule,
+        AsyncPipe,
+    ],
     templateUrl: './inicio.html',
+    styleUrl: './inicio.scss'
 })
-export class Inicio { }
+export class Inicio {
+    protected readonly usuarioAutenticado$ = inject(AuthService)
+        .obterAccessToken()
+        .pipe(map((x) => x?.usuarioAutenticado));
+}
