ac75ca70db86c54577c23a245bb71dc39423dcb3
Gustavo Duara <gugaduara@gmail.com>
2026-09-14 19:09:12 -0300

    criado ModuloFuncionario na infra Orm

---
+++ Server/LocadoraVeiculos.Infraestrutura.Orm/ModuloFuncionario/MapeadorFuncionarioEmOrm.cs
@@ -0,0 +1,43 @@
+﻿using LocadoraDeVeiculos.Dominio.ModuloFuncionario;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+public class MapeadorFuncionarioEmOrm : IEntityTypeConfiguration<Funcionario>
+{
+    public void Configure(EntityTypeBuilder<Funcionario> builder)
+    {
+        builder.HasKey(f => f.Id);
+
+        builder.Property(c => c.NomeCompleto)
+               .HasColumnType("nvarchar(100)")
+               .IsRequired();
+
+        builder.Property(c => c.Cpf)
+               .HasColumnType("varchar(14)")
+               .IsRequired();
+
+        builder.Property(c => c.Email)
+               .HasColumnType("varchar(100)")
+               .IsRequired();
+
+        builder.Property(c => c.Salario)
+               .HasColumnType("decimal(18,2)")
+               .IsRequired();
+
+        builder.Property(c => c.AdmissaoEmUtc)
+               .HasColumnType("datetimeoffset")
+               .IsRequired();
+
+        builder.HasOne(c => c.Usuario)
+               .WithOne()
+               .HasForeignKey<Funcionario>(f => f.UsuarioId)
+               .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasOne(c => c.Empresa)
+               .WithMany()
+               .HasForeignKey(f => f.EmpresaId)
+               .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasIndex(f => new { f.EmpresaId, f.Excluido });
+    }
+}
---
+++ Server/LocadoraVeiculos.Infraestrutura.Orm/ModuloFuncionario/RepositorioFuncionarioEmOrm.cs
@@ -0,0 +1,40 @@
+﻿using LocadoraDeVeiculos.Dominio.ModuloFuncionario;
+using LocadoraDeVeiculos.Infraestrutura.Orm.Compartilhado;
+using Microsoft.EntityFrameworkCore;
+
+namespace LocadoraDeVeiculos.Infraestrutura.Orm.ModuloFuncionario;
+
+public class RepositorioFuncionarioEmOrm : RepositorioBaseEmOrm<Funcionario>
+{
+    private readonly AppDbContext dbContext;
+
+    public RepositorioFuncionarioEmOrm(AppDbContext dbContext) : base(dbContext)
+    {
+        this.dbContext = dbContext;
+    }
+
+    public override async Task<Funcionario?> SelecionarPorIdAsync(Guid funcionarioId)
+    {
+        return await dbContext.Funcionarios
+            .Include(u => u.Empresa)
+            .Include(u => u.Usuario)
+            .FirstOrDefaultAsync(f => f.Id == funcionarioId);
+    }
+
+    public async Task<Funcionario?> SelecionarPorUsuarioIdAsync(Guid usuarioId)
+    {
+        return await dbContext.Funcionarios
+            .IgnoreQueryFilters()
+            .Include(u => u.Empresa)
+            .Include(u => u.Usuario)
+            .FirstOrDefaultAsync(f => f.UsuarioId == usuarioId);
+    }
+
+    public override async Task<List<Funcionario>> SelecionarTodosAsync()
+    {
+        return await dbContext.Funcionarios
+            .Include(u => u.Empresa)
+            .Include(u => u.Usuario)
+            .ToListAsync();
+    }
+}
