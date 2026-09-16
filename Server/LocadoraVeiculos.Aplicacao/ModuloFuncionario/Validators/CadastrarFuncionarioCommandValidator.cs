751955d50c9d0ce8253ea639002434567bf72772
Gustavo Duara <gugaduara@gmail.com>
2026-09-16 15:15:46 -0300

    feito Validação de cadastro e edição de usuario

---
+++ Server/LocadoraVeiculos.Aplicacao/ModuloFuncionario/Validators/CadastrarFuncionarioCommandValidator.cs
@@ -0,0 +1,38 @@
+﻿using FluentValidation;
+using LocadoraDeVeiculos.Aplicacao.ModuloFuncionario.Commands;
+
+namespace LocadoraDeVeiculos.Aplicacao.ModuloFuncionario.Validators;
+
+public class CadastrarFuncionarioCommandValidator : AbstractValidator<CadastrarFuncionarioCommand>
+{
+    public CadastrarFuncionarioCommandValidator()
+    {
+        RuleFor(p => p.NomeCompleto)
+            .NotEmpty().WithMessage("O campo {PropertyName} é obrigatório.")
+            .MinimumLength(3).WithMessage("O campo {PropertyName} deve conter no mínimo {MinLength} caracteres.");
+
+        RuleFor(p => p.Cpf)
+            .NotEmpty().WithMessage("O campo {PropertyName} é obrigatório.")
+            .Matches(@"^\d{3}\.\d{3}\.\d{3}-\d{2}$")
+            .WithMessage("O campo {PropertyName} deve seguir o formato '000.000.000-00'.");
+
+        RuleFor(p => p.Email)
+            .NotEmpty().WithMessage("O campo {PropertyName} é obrigatório.")
+            .EmailAddress().WithMessage("O campo {PropertyName} deve conter um endereço de e-mail válido.");
+
+        RuleFor(p => p.Senha)
+            .NotEmpty().WithMessage("O campo {PropertyName} é obrigatório.")
+            .MinimumLength(6).WithMessage("A senha deve conter pelo menos {MinLength} caracteres.") // options.Password.RequiredLength = 6
+            .Matches(@"[A-Z]").WithMessage("A senha deve conter pelo menos uma letra maiúscula.")   // options.Password.RequireUppercase = true
+            .Matches(@"[a-z]").WithMessage("A senha deve conter pelo menos uma letra minúscula.")   // options.Password.RequireLowercase = true
+            .Matches(@"[0-9]").WithMessage("A senha deve conter pelo menos um número.");            // options.Password.RequireDigit = true
+
+        RuleFor(p => p.ConfirmarSenha)
+            .NotEmpty().WithMessage("O campo {PropertyName} é obrigatório.")
+            .Equal(p => p.Senha).WithMessage("As senhas não conferem.");
+
+        RuleFor(p => p.Salario)
+            .NotEmpty().WithMessage("O campo {PropertyName} é obrigatório.")
+            .GreaterThan(0).WithMessage("O campo {PropertyName} precisa conter um valor maior que 0.");
+    }
+}
---
+++ Server/LocadoraVeiculos.Aplicacao/ModuloFuncionario/Validators/EditarFuncionarioCommandValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using LocadoraDeVeiculos.Aplicacao.ModuloFuncionario.Commands;
+
+namespace LocadoraDeVeiculos.Aplicacao.ModuloFuncionario.Validators;
+
+public class EditarFuncionarioCommandValidator : AbstractValidator<EditarFuncionarioCommand>
+{
+    public EditarFuncionarioCommandValidator()
+    {
+        RuleFor(p => p.NomeCompleto)
+            .NotEmpty().WithMessage("O campo {PropertyName} é obrigatório.")
+            .MinimumLength(3).WithMessage("O campo {PropertyName} deve conter no mínimo {MinLength} caracteres.");
+
+        RuleFor(p => p.Cpf)
+            .NotEmpty().WithMessage("O campo {PropertyName} é obrigatório.")
+            .Matches(@"^\d{3}\.\d{3}\.\d{3}-\d{2}$")
+            .WithMessage("O campo {PropertyName} deve seguir o formato '000.000.000-00'.");
+
+        RuleFor(p => p.Salario)
+            .NotEmpty().WithMessage("O campo {PropertyName} é obrigatório.")
+            .GreaterThan(0).WithMessage("O campo {PropertyName} precisa conter um valor maior que 0.");
+    }
+}
