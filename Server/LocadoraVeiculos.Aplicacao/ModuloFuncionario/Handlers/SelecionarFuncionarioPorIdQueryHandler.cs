279d74be1d0f2d5f5761f23bcee99288ee2ca067
Gustavo Duara <gugaduara@gmail.com>
2026-09-16 15:04:38 -0300

    feito seleção de funcionario QueryHandler

---
+++ Server/LocadoraVeiculos.Aplicacao/ModuloFuncionario/Handlers/SelecionarFuncionarioPorIdQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using FluentResults;
+using LocadoraDeVeiculos.Aplicacao.Compartilhado;
+using LocadoraDeVeiculos.Aplicacao.ModuloFuncionario.Commands;
+using LocadoraDeVeiculos.Dominio.ModuloFuncionario;
+using LocadoraDeVeiculos.Infraestrutura.Orm.ModuloFuncionario;
+using MediatR;
+
+namespace LocadoraDeVeiculos.Aplicacao.ModuloFuncionario.Handlers;
+
+public class SelecionarFuncionarioPorIdQueryHandler(RepositorioFuncionarioEmOrm repositorioFuncionario)
+    : IRequestHandler<SelecionarFuncionarioPorIdQuery, Result<SelecionarFuncionarioPorIdResult>>
+{
+    public async Task<Result<SelecionarFuncionarioPorIdResult>> Handle(
+        SelecionarFuncionarioPorIdQuery query, CancellationToken cancellationToken)
+    {
+        Funcionario? registroEncontrado = await repositorioFuncionario.SelecionarPorIdAsync(query.Id);
+
+        if (registroEncontrado is null)
+            return Result.Fail(ResultadosErro.RegistroNaoEncontradoErro(query.Id));
+
+        SelecionarFuncionarioPorIdResult result = new(
+            registroEncontrado.Id,
+            registroEncontrado.NomeCompleto,
+            registroEncontrado.Email,
+            registroEncontrado.Salario,
+            registroEncontrado.AdmissaoEmUtc
+        );
+
+        return Result.Ok(result);
+    }
+}
---
+++ Server/LocadoraVeiculos.Aplicacao/ModuloFuncionario/Handlers/SelecionarFuncionariosQueryHandler.cs
@@ -0,0 +1,32 @@
+﻿using FluentResults;
+using LocadoraDeVeiculos.Aplicacao.ModuloFuncionario.Commands;
+using LocadoraDeVeiculos.Infraestrutura.Orm.ModuloFuncionario;
+using MediatR;
+using System.Collections.Immutable;
+
+namespace LocadoraDeVeiculos.Aplicacao.ModuloFuncionario.Handlers;
+
+public class SelecionarFuncionariosQueryHandler(
+    RepositorioFuncionarioEmOrm repositorioFuncionario
+) : IRequestHandler<SelecionarFuncionariosQuery, Result<SelecionarFuncionariosResult>>
+{
+    public async Task<Result<SelecionarFuncionariosResult>> Handle(
+        SelecionarFuncionariosQuery query, CancellationToken cancellationToken)
+    {
+        var registros = await repositorioFuncionario.SelecionarTodosAsync();
+
+        var dtos = registros
+            .Select(r => new SelecionarFuncionariosDto(
+                r.Id,
+                r.NomeCompleto,
+                r.Email,
+                r.Salario,
+                r.AdmissaoEmUtc
+            ))
+            .ToImmutableList();
+
+        var response = new SelecionarFuncionariosResult(dtos);
+
+        return Result.Ok(response);
+    }
+}
