fbc0c89a0e1c968c17846c03a0162f7b3c231436
Gustavo Duara <gugaduara@gmail.com>
2026-09-16 15:02:39 -0300

    feito exclusao e edição de funcionario CommandHandler

---
+++ Server/LocadoraVeiculos.Aplicacao/ModuloFuncionario/Handlers/EditarFuncionarioCommandHandler.cs
@@ -0,0 +1,81 @@
+﻿using FluentResults;
+using FluentValidation;
+using FluentValidation.Results;
+using LocadoraDeVeiculos.Aplicacao.Compartilhado;
+using LocadoraDeVeiculos.Aplicacao.ModuloFuncionario.Commands;
+using LocadoraDeVeiculos.Dominio.ModuloAutenticacao;
+using LocadoraDeVeiculos.Dominio.ModuloFuncionario;
+using LocadoraDeVeiculos.Infraestrutura.Orm.Compartilhado;
+using LocadoraDeVeiculos.Infraestrutura.Orm.ModuloFuncionario;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace LocadoraDeVeiculos.Aplicacao.ModuloFuncionario.Handlers;
+
+public class EditarFuncionarioCommandHandler(
+    AppDbContext appDbContext,
+    RepositorioFuncionarioEmOrm repositorioFuncionario,
+    ITenantProvider tenantProvider,
+    IValidator<EditarFuncionarioCommand> validator,
+    ILogger<EditarFuncionarioCommandHandler> logger
+) : IRequestHandler<EditarFuncionarioCommand, Result<EditarFuncionarioResult>>
+{
+    public async Task<Result<EditarFuncionarioResult>> Handle(
+        EditarFuncionarioCommand command, CancellationToken cancellationToken)
+    {
+        Funcionario? registroEncontrado = await repositorioFuncionario.SelecionarPorIdAsync(command.Id);
+
+        if (registroEncontrado is null)
+            return Result.Fail(ResultadosErro.RegistroNaoEncontradoErro(command.Id));
+
+        ValidationResult resultadoValidacao = await validator.ValidateAsync(command, cancellationToken);
+
+        if (!resultadoValidacao.IsValid)
+        {
+            var erros = resultadoValidacao.Errors.Select(e => e.ErrorMessage);
+
+            return Result.Fail(ResultadosErro.RequisicaoInvalidaErro(erros));
+        }
+
+        List<Funcionario> registros = await repositorioFuncionario.SelecionarTodosAsync();
+
+        if (registros.Any(x => !x.Id.Equals(command.Id) && x.Cpf.Equals(command.Cpf)))
+            return Result.Fail(ResultadosErro.RegistroDuplicadoErro("Um funcionário com este CPF já está cadastrado."));
+
+        try
+        {
+            Funcionario funcionarioEditado = new(
+                registroEncontrado.UsuarioId,
+                tenantProvider.EmpresaId.GetValueOrDefault(),
+                command.NomeCompleto,
+                command.Cpf,
+                registroEncontrado.Email,
+                command.Salario,
+                command.AdmissaoEmUtc
+            );
+
+            await repositorioFuncionario.EditarAsync(command.Id, funcionarioEditado);
+
+            await appDbContext.SaveChangesAsync(cancellationToken);
+
+            EditarFuncionarioResult result = new(
+                command.NomeCompleto,
+                command.Cpf,
+                command.Salario,
+                command.AdmissaoEmUtc
+            );
+
+            return Result.Ok(result);
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(
+                ex,
+                "Ocorreu um erro durante a edição de {@Command}.",
+                command
+            );
+
+            return Result.Fail(ResultadosErro.ExcecaoInternaErro(ex));
+        }
+    }
+}
---
+++ Server/LocadoraVeiculos.Aplicacao/ModuloFuncionario/Handlers/ExcluirFuncionarioCommandHandler.cs
@@ -0,0 +1,55 @@
+﻿using FluentResults;
+using LocadoraDeVeiculos.Aplicacao.Compartilhado;
+using LocadoraDeVeiculos.Aplicacao.ModuloFuncionario.Commands;
+using LocadoraDeVeiculos.Dominio.ModuloAutenticacao;
+using LocadoraDeVeiculos.Dominio.ModuloFuncionario;
+using LocadoraDeVeiculos.Infraestrutura.Jwt.Services;
+using LocadoraDeVeiculos.Infraestrutura.Orm.Compartilhado;
+using LocadoraDeVeiculos.Infraestrutura.Orm.ModuloFuncionario;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+
+namespace LocadoraDeVeiculos.Aplicacao.ModuloFuncionario.Handlers;
+
+public class ExcluirFuncionarioCommandHandler(
+    AppDbContext dbContext,
+    RepositorioFuncionarioEmOrm repositorioFuncionario,
+    RefreshTokenProvider refreshTokenProvider,
+    UserManager<Usuario> userManager,
+    ILogger<ExcluirFuncionarioCommandHandler> logger
+) : IRequestHandler<ExcluirFuncionarioCommand, Result<ExcluirFuncionarioResult>>
+{
+    public async Task<Result<ExcluirFuncionarioResult>> Handle(
+        ExcluirFuncionarioCommand command, CancellationToken cancellationToken)
+    {
+        try
+        {
+            Funcionario? funcionario = await repositorioFuncionario.SelecionarPorIdAsync(command.Id);
+
+            if (funcionario is null)
+                return Result.Fail(ResultadosErro.RegistroNaoEncontradoErro(command.Id));
+
+            Usuario? usuario = await userManager.FindByIdAsync(funcionario.UsuarioId.ToString());
+
+            if (usuario is not null)
+                await refreshTokenProvider.RevogarTokensUsuarioAsync(usuario.Id, "Exclusão");
+
+            await repositorioFuncionario.ExcluirAsync(funcionario.Id);
+
+            await dbContext.SaveChangesAsync(cancellationToken);
+
+            return Result.Ok(new ExcluirFuncionarioResult());
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(
+                ex,
+                "Ocorreu um erro durante a exclusão de de {@Command}.",
+                command
+            );
+
+            return Result.Fail(ResultadosErro.ExcecaoInternaErro(ex));
+        }
+    }
+}
