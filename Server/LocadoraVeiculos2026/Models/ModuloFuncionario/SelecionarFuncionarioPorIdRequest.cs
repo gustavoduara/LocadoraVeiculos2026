e4170ca064e9b344e0b76930f7b5161b109b173d
Gustavo Duara <gugaduara@gmail.com>
2026-09-18 15:11:43 -0300

    (server) feito seleção de funcionario Models

---
+++ Server/LocadoraVeiculos2026/Models/ModuloFuncionario/SelecionarFuncionarioPorIdRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace LocadoraDeVeiculos.WebApi.Models.ModuloFuncionario;
+
+public record SelecionarFuncionarioPorIdResponse(
+    Guid Id,
+    string NomeCompleto,
+    string Email,
+    decimal Salario,
+    DateTimeOffset AdmissaoEmUtc
+);
---
+++ Server/LocadoraVeiculos2026/Models/ModuloFuncionario/SelecionarFuncionariosRequest.cs
@@ -0,0 +1,7 @@
+﻿using LocadoraDeVeiculos.Aplicacao.ModuloFuncionario.Commands;
+
+namespace LocadoraDeVeiculos.WebApi.Models.ModuloFuncionario;
+
+public record SelecionarFuncionariosRequest();
+
+public record SelecionarFuncionariosResponse(IReadOnlyList<SelecionarFuncionariosDto> Registros);
