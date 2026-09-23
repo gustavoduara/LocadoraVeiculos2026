3180c01c768fae9880595b791dde44aa20a470d6
Gustavo Duara <gugaduara@gmail.com>
2026-09-22 23:16:41 -0300

    (server) adicionado SeleçãoQuery grupoVeiculos na aplicação

---
+++ Server/LocadoraVeiculos.Aplicacao/ModuloGrupoVeiculos/Commands/SelecionarGrupoVeiculosIdQuery.cs
@@ -0,0 +1,8 @@
+﻿using FluentResults;
+using MediatR;
+
+namespace LocadoraDeVeiculos.Aplicacao.ModuloGrupoVeiculos.Commands;
+
+public record SelecionarGrupoVeiculosPorIdQuery(Guid Id) : IRequest<Result<SelecionarGrupoVeiculosPorIdResult>>;
+
+public record SelecionarGrupoVeiculosPorIdResult(Guid Id, string Nome);
---
+++ Server/LocadoraVeiculos.Aplicacao/ModuloGrupoVeiculos/Commands/SelecionarGrupoVeiculosQuery.cs
@@ -0,0 +1,11 @@
+﻿using FluentResults;
+using MediatR;
+
+namespace LocadoraDeVeiculos.Aplicacao.ModuloGrupoVeiculos.Commands;
+
+public record SelecionarGruposVeiculosQuery()
+    : IRequest<Result<SelecionarGruposVeiculosResult>>;
+
+public record SelecionarGruposVeiculosResult(IReadOnlyList<SelecionarGruposVeiculosDto> Registros);
+
+public record SelecionarGruposVeiculosDto(Guid Id, string Nome);
