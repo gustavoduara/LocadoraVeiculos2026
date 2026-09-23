97966d213ef036f03e40950c6ec16bd64fc74fac
Gustavo Duara <gugaduara@gmail.com>
2026-09-23 19:03:59 -0300

    (server)criado validators GrupoVeiculos

---
+++ Server/LocadoraVeiculos.Aplicacao/ModuloGrupoVeiculos/Validator/CadastrarGrupoVeiculosCommandValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using LocadoraDeVeiculos.Aplicacao.ModuloGrupoVeiculos.Commands;
+
+namespace LocadoraDeVeiculos.Aplicacao.ModuloGrupoVeiculos.Validators;
+
+public class CadastrarGrupoVeiculosCommandValidator
+    : AbstractValidator<CadastrarGrupoVeiculosCommand>
+{
+    public CadastrarGrupoVeiculosCommandValidator()
+    {
+        RuleFor(m => m.Nome)
+            .NotEmpty().WithMessage("O campo {PropertyName} é obrigatório")
+            .DependentRules(() =>
+            {
+                RuleFor(m => m.Nome).MinimumLength(3)
+                    .WithMessage("O campo {PropertyName} deve conter no mínimo {MinLength} caracteres");
+            });
+    }
+}
---
+++ Server/LocadoraVeiculos.Aplicacao/ModuloGrupoVeiculos/Validator/EditarGrupoVeiculosCommandValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using LocadoraDeVeiculos.Aplicacao.ModuloGrupoVeiculos.Commands;
+
+namespace LocadoraDeVeiculos.Aplicacao.ModuloGrupoVeiculos.Validators;
+
+public class EditarGrupoVeiculosCommandValidator
+    : AbstractValidator<EditarGrupoVeiculosCommand>
+{
+    public EditarGrupoVeiculosCommandValidator()
+    {
+        RuleFor(m => m.Nome)
+            .NotEmpty().WithMessage("O campo {PropertyName} é obrigatório")
+            .DependentRules(() =>
+            {
+                RuleFor(m => m.Nome).MinimumLength(3)
+                    .WithMessage("O campo {PropertyName} deve conter no mínimo {MinLength} caracteres");
+            });
+    }
+}
