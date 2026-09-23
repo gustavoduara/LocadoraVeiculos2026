1ea0f787820d3666a7276fc5c209205699ab6685
Gustavo Duara <gugaduara@gmail.com>
2026-09-23 19:00:18 -0300

    (server)criado Seleçao grupo veiculos handler

---
+++ Server/LocadoraVeiculos.Aplicacao/ModuloGrupoVeiculos/Handlers/SelecionarGrupoVeiculosPorIdQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using FluentResults;
+using LocadoraDeVeiculos.Aplicacao.ModuloGrupoVeiculos.Commands;
+using LocadoraDeVeiculos.Infraestrutura.Orm.ModuloGrupoVeiculos;
+using MediatR;
+using System.Collections.Immutable;
+
+namespace LocadoraDeVeiculos.Aplicacao.ModuloGrupoVeiculos.Handlers;
+
+public class SelecionarGruposVeiculosQueryHandler(
+    RepositorioGrupoVeiculosEmOrm repositorioGrupoVeiculos
+) : IRequestHandler<SelecionarGruposVeiculosQuery, Result<SelecionarGruposVeiculosResult>>
+{
+    public async Task<Result<SelecionarGruposVeiculosResult>> Handle(
+        SelecionarGruposVeiculosQuery request, CancellationToken cancellationToken)
+    {
+        var registros = await repositorioGrupoVeiculos
+            .SelecionarTodosAsync();
+
+        var dtos = registros
+            .Select(r => new SelecionarGruposVeiculosDto(
+                r.Id,
+                r.Nome
+            ))
+            .ToImmutableList();
+
+        var response = new SelecionarGruposVeiculosResult(dtos);
+
+        return Result.Ok(response);
+    }
+}
---
+++ Server/LocadoraVeiculos.Aplicacao/ModuloGrupoVeiculos/Handlers/SelecionarGruposVeiculosQueryHandler.cs
@@ -0,0 +1,28 @@
+﻿using FluentResults;
+using LocadoraDeVeiculos.Aplicacao.Compartilhado;
+using LocadoraDeVeiculos.Aplicacao.ModuloGrupoVeiculos.Commands;
+using LocadoraDeVeiculos.Dominio.ModuloGrupoVeiculos;
+using LocadoraDeVeiculos.Infraestrutura.Orm.ModuloGrupoVeiculos;
+using MediatR;
+
+namespace LocadoraDeVeiculos.Aplicacao.ModuloGrupoVeiculos.Handlers;
+
+public class SelecionarGrupoVeiculosPorIdQueryHandler(RepositorioGrupoVeiculosEmOrm repositorioGrupoVeiculos)
+    : IRequestHandler<SelecionarGrupoVeiculosPorIdQuery, Result<SelecionarGrupoVeiculosPorIdResult>>
+{
+    public async Task<Result<SelecionarGrupoVeiculosPorIdResult>> Handle(
+        SelecionarGrupoVeiculosPorIdQuery query, CancellationToken cancellationToken)
+    {
+        GrupoVeiculos? registroEncontrado = await repositorioGrupoVeiculos.SelecionarPorIdAsync(query.Id);
+
+        if (registroEncontrado is null)
+            return Result.Fail(ResultadosErro.RegistroNaoEncontradoErro(query.Id));
+
+        SelecionarGrupoVeiculosPorIdResult response = new(
+            query.Id,
+            registroEncontrado.Nome
+        );
+
+        return Result.Ok(response);
+    }
+}
